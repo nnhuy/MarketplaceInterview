354995c845f7e0a14e03abdcf355ce26f2753d35
nnhuy <nnhuy@sdc.ud.edu.vn>
2015-05-20 21:17:56 +0700

    commit finish code part 1

---
+++ Marketplace.Interview/Marketplace.Interview.Business/Shipping/PerRegionAnotherShipping.cs
@@ -0,0 +1,17 @@
+﻿using Marketplace.Interview.Business.Basket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marketplace.Interview.Business.Shipping
+{
+   public class PerRegionAnotherShipping : PerRegionShipping
+    {
+       public override string GetDescription(LineItem lineItem, Basket.Basket basket)
+       {
+           return string.Format("Shipping to {0} (*)", lineItem.DeliveryRegion);
+       }
+    }
+}
